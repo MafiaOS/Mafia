f1568f465bc270a2d4a9dca5cce21b3e1bf41584
DEVIL ULTRA <107530377+DevilUltra@users.noreply.github.com>
2022-07-03 14:30:02 +0530

    Update var.py

--- var.py
+++ var.py
@@ -1,1 +1,11 @@
-godss
+import os
+
+ENV = bool(os.environ.get("ENV", "ANYTHING"))
+if ENV:
+    from heroku_config import Var as config
+else:
+    from localconfig import config
+
+
+Var = config
+
