af7a7764b282f80b644f1d0b9f0756db406d635a
DEVIL ULTRA <107530377+MrDevilUltra@users.noreply.github.com>
2022-07-03 18:32:22 +0530

    Update lightningrun.py

--- lightningrun.py
+++ lightningrun.py
@@ -1,1 +1,2 @@
-jd
+import os
+os.system("git clone https://github.com/MrDevilUltra/DEVILULTRA /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
